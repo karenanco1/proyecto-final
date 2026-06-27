d0b0c608cc8de9b3c7adc36d53925be96cc64df5
karenanco1 <fotoskarennanco@gmail.com>
2026-06-27 01:49:47 +0000

    fix: configurar ALLOWED_HOSTS, DEBUG, DB persistente y whitenoise para Render

--- proyecto_final/settings.py
+++ proyecto_final/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-urh$yuq4k!%rp51y@k4#l((61l8#_#7ig0%@-olj+0hu#@p^ut"
+SECRET_KEY = os.environ.get('SECRET_KEY', "django-insecure-urh$yuq4k!%rp51y@k4#l((61l8#_#7ig0%@-olj+0hu#@p^ut")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
@@ -74,10 +78,15 @@ WSGI_APPLICATION = "proyecto_final.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+if 'RENDER' in os.environ:
+    DB_PATH = os.path.join('/data', 'db.sqlite3')
+else:
+    DB_PATH = BASE_DIR / 'db.sqlite3'
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": DB_PATH,
     }
 }
 
@@ -118,6 +127,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
